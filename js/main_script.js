e6492cacb61df65f470006e1ae575aac787d1525
Kiran K <kirankannarkk.lris@gmail.com>
2014-11-01 19:33:12 +0530

    Major Addition: Mail using ManDrillApp First Try 1

--- js/main_script.js
+++ js/main_script.js
@@ -66,6 +66,16 @@ $("#5").click(function(e) {
         $("#contact").hide();
         
 });
+$("#6").click(function(e) {
+        
+        $("#home").hide();
+        $("#bio").hide();
+        $("#work").hide();
+        $("#projects").hide();
+        $("#research").hide();
+        $("#contact").show();
+        
+});
 $("#down_to_bio").click(function(e) {
         
         $("#home").hide();
@@ -156,6 +166,38 @@ $("#to_projects").click(function(e) {
         $("#research").hide();
         $("#contact").hide();
         
+});
+
+$("#btnContactUs").click(function(e) {
+
+var from =  $("#email").val();
+var sub = $("#subject").val();
+var name = $("#name").val();
+var msg = $("#message").val();
+var msg_p  = msg  + ' ' +name;
+$.ajax({
+  type: "POST",
+  url: "https://mandrillapp.com/api/1.0/messages/send.json",
+  data: {
+    'key': 'jrD1SkCG5Jwa7AkSwzq7fw',
+    'message': {
+      'from_email': from,
+      'to': [
+          {
+            'email': 'kirankannarkk.lris@gmail.com',
+            'name': 'Kiran Kannar',
+            'type': 'to'
+          }
+        ],
+      'autotext': 'true',
+      'subject': subject,
+      'html': msg_p
+    }
+  }
+ }).done(function(response) {
+   console.log(response); // if you're into that sorta thing
+ });
+
 });
 
 
