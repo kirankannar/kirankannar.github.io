b4a6ddd3582c30092790664882a7767ea283fa7c
Kiran K <kirankannarkk.lris@gmail.com>
2014-11-01 18:55:01 +0530

    Checkin nav bar. Removed sidebar. Also added up/down arrows

--- js/main_script.js
+++ js/main_script.js
@@ -13,4 +13,120 @@ $(document).ready(function() {
 $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("active");
+        
 });
+
+$("#1").click(function(e) {
+        
+        $("#home").show();
+        $("#bio").hide();
+        $("#work").hide();
+        $("#projects").hide();
+        $("#research").hide();
+        $("#contact").hide();
+        
+});
+$("#2").click(function(e) {
+          
+        $("#home").hide();
+        $("#bio").show();
+        $("#work").hide();
+        $("#projects").hide();
+        $("#research").hide();
+        $("#contact").hide();
+        
+});
+$("#3").click(function(e) {
+        
+        $("#home").hide();
+        $("#bio").hide();
+        $("#work").show();
+        $("#projects").hide();
+        $("#research").hide();
+        $("#contact").hide();
+        
+});
+$("#4").click(function(e) {
+        
+        $("#home").hide();
+        $("#bio").hide();
+        $("#work").hide();
+        $("#projects").show();
+        $("#research").hide();
+        $("#contact").hide();
+        
+});
+$("#5").click(function(e) {
+        
+        $("#home").hide();
+        $("#bio").hide();
+        $("#work").hide();
+        $("#projects").hide();
+        $("#research").show();
+        $("#contact").hide();
+        
+});
+$("#down_to_bio").click(function(e) {
+        
+        $("#home").hide();
+        $("#bio").show();
+        $("#work").hide();
+        $("#projects").hide();
+        $("#research").hide();
+        $("#contact").hide();
+        
+});
+$("#down_to_work").click(function(e) {
+        
+        $("#home").hide();
+        $("#bio").hide();
+        $("#work").show();
+        $("#projects").hide();
+        $("#research").hide();
+        $("#contact").hide();
+        
+});
+$("#down_to_projects").click(function(e) {
+        
+        $("#home").hide();
+        $("#bio").hide();
+        $("#work").hide();
+        $("#projects").show();
+        $("#research").hide();
+        $("#contact").hide();
+        
+});
+$("#down_to_research").click(function(e) {
+        
+        $("#home").hide();
+        $("#bio").hide();
+        $("#work").hide();
+        $("#projects").hide();
+        $("#research").show();
+        $("#contact").hide();
+        
+});
+$("#down_to_contact").click(function(e) {
+        
+        $("#home").hide();
+        $("#bio").hide();
+        $("#work").hide();
+        $("#projects").hide();
+        $("#research").hide();
+        $("#contact").show();
+        
+});
+
+$("#to_home").click(function(e) {
+        
+        $("#home").show();
+        $("#bio").hide();
+        $("#work").hide();
+        $("#projects").hide();
+        $("#research").hide();
+        $("#contact").hide();
+        
+});
+
+
+
