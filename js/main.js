aab610654d1dc0467928b9dd6599249a7f3e2d16
Kiran Kannar <kirankannarkk.lris@gmail.com>
2015-11-08 17:27:33 +0530

    Adding preloader

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
+$(window).load(function() {
+        $(".pre-load").fadeOut("slow");
+    });
+
 $("#menu").click(function(){
 
-  var popup = document.getElementById("menu_popup");
-  popup.setAttribute("class", "overlay");
-  var menuicon = document.getElementById("menu");
-  menuicon.setAttribute("class","navicon navicon_hidden");
-  
-
+  $("#menu_popup").attr("class","overlay");
+  $("#menu").attr("class","navicon navicon_hidden");
 });
 $("#menu_close").click(function(){
 
-  var popup = document.getElementById("menu_popup");
-  popup.setAttribute("class", "overlay o_hidden");
-  var menuicon = document.getElementById("menu");
-  menuicon.setAttribute("class","navicon");
+  $("#menu_popup").attr("class","overlay o_hidden");
+  $("#menu").attr("class","navicon");
 });
 
 $("#menulist").on('click', 'li', function() {
-  var popup = document.getElementById("menu_popup");
-  popup.setAttribute("class", "overlay o_hidden");
-  var menuicon = document.getElementById("menu");
-  menuicon.setAttribute("class","navicon"); 
+
+  $("#menu_popup").attr("class","overlay o_hidden");
+  $("#menu").attr("class","navicon");
 });
