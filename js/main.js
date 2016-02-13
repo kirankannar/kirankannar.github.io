d158613a089d7e7da78bf5992d7a045db99d78a4
Kiran Kannar <kirankannarkk.lris@gmail.com>
2016-02-13 19:56:17 +0530

    Write page ui change

--- js/main.js
+++ js/main.js
@@ -6,15 +6,18 @@ $("#menu").click(function(){
 
   $("#menu_popup").attr("class","overlay");
   $("#menu").attr("class","navicon navicon_hidden");
+  $("body").css("overflow","hidden");
 });
 $("#menu_close").click(function(){
 
   $("#menu_popup").attr("class","overlay o_hidden");
   $("#menu").attr("class","navicon");
+  $("body").css("overflow","");
 });
 
 $("#menulist").on('click', 'li', function() {
 
   $("#menu_popup").attr("class","overlay o_hidden");
   $("#menu").attr("class","navicon");
+  $("body").css("overflow","");
 });
